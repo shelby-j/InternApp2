532c8247d8f21a0bae53d89508ffc13988410beb
Shelby Jones <shelby-j@users.noreply.github.com>
2020-06-10 16:50:12 -0500

    Added DbContext and todo model

---
+++ InternApp2/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace InternApp2.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Models.TodoItem> TodoItems { get; set; }
+    }
+
+
+}
--- InternApp2/Startup.cs
+++ InternApp2/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using InternApp2.Models;
 
 namespace InternApp2
 {
@@ -25,6 +27,7 @@ namespace InternApp2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlite("TodoList"));
             services.AddControllers();
         }
 
